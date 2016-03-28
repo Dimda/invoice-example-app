8ff453ae45ec2fca87fb198d27952c9749a7bc06
Davis Kemlers <daviskemlers@gmail.com>
2016-03-28 13:51:21 +0900

    refactured invoice tickets querry

--- collections.js
+++ collections.js
@@ -1,1 +1,29 @@
 InvoiceTickets = new Mongo.Collection('invoiceTickets');
+
+InvoiceTickets.byTimeRange = function(filter, sortBy, sortOrder){
+  let sortQuery = {};
+  sortQuery[sortBy] = sortOrder;
+
+  let startDate;
+  let endDate = new Date();
+  switch (filter) {
+    default:
+    case "today":
+      startDate = new Date(endDate.setDate(endDate.getDate()-1));
+      break;
+    case "week":
+      startDate = new Date(endDate.setDate(endDate.getDate()-7));
+      break;
+    case "month":
+      startDate = new Date(endDate.setDate(endDate.getDate()-30));
+      break;
+    case "all":
+      return InvoiceTickets.find({}, {sort: sortQuery});
+      break;
+  }
+
+  return InvoiceTickets.find(
+    {"createdAt": {$gte: startDate}},
+    {sort: sortQuery}
+  );
+};
