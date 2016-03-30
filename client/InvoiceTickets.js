2066dd09c4fee0dd2f93a720fddcee82bef1e337
Davis Kemlers <daviskemlers@gmail.com>
2016-03-30 14:27:35 +0900

    switched form using session variables to using url parameters

--- client/InvoiceTickets.js
+++ client/InvoiceTickets.js
@@ -1,49 +1,19 @@
 Meteor.subscribe("invoiceTickets");
 //Setting default session values
-Session.set("filter", "today");
-Session.set("sort-by", "createdAt");
-Session.set("sort-order", 1);
 
 Template.registerHelper('equals', function(a, b){
-  return a === b;
+  return a == b;
 });
 
 Template.InvoiceTickets.helpers({
   tickets: function(){
-    return InvoiceTickets.byTimeRange(Session.get('filter'), Session.get('sort-by'), Session.get('sort-order'));
+    return InvoiceTickets.byTimeRange(
+      FlowRouter.getParam("filter"),
+      FlowRouter.getQueryParam("sortBy"),
+      FlowRouter.getQueryParam("sortOrder")
+    );
   },
   formatDate: function(date){
     return moment(date).format("MM-DD-YYYY");
-  },
-});
-
-Template.InvoiceTickets.events({
-  "click .today": function(event, template){
-     Session.set("filter", "today");
-  },
-  "click .week": function(event, template){
-     Session.set("filter", "week");
-  },
-  "click .month": function(event, template){
-     Session.set("filter", "month");
-  },
-  "click .all": function(event, template){
-     Session.set("filter", "all");
-  },
-  "click .total-sort": function(event, template){
-    if(Session.get("sort-order") == 1){
-      Session.set("sort-order", -1);
-    }else{
-      Session.set("sort-order", 1);
-    }
-    Session.set("sort-by", "total");
-  },
-  "click .created-at-sort": function(event, template){
-     if(Session.get("sort-order") == 1){
-       Session.set("sort-order", -1);
-     }else{
-       Session.set("sort-order", 1);
-     }
-     Session.set("sort-by", "createdAt");
   },
 });
