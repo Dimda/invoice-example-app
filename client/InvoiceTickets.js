7bf997dd35be401bd6baee5629d4c5bd058e6f8f
Davis Kemlers <daviskemlers@gmail.com>
2016-04-06 17:04:04 +0900

    replaced target.date with jQuery event

--- client/InvoiceTickets.js
+++ client/InvoiceTickets.js
@@ -29,30 +29,29 @@ Template.InvoiceTickets.helpers({
   formatDate: function(date){
     return moment(date).format("MM-DD-YYYY");
   },
-});
-Template.InvoiceTickets.moreResults = function() {
+  moreResults: function(){
     // If, once the subscription is ready, we have less rows than we
     // asked for, we've got all the rows in the collection.
     return !(InvoiceTickets.find().count() < Session.get("itemsLimit"));
-}
+  }
+});
+
+Template.InvoiceTickets.events({
+  "becameVisible .showMoreResults": function(event, template){
+    Session.set("itemsLimit",
+    Session.get("itemsLimit") + ITEMS_INCREMENT);
+  }
+});
 
 // whenever #showMoreResults becomes visible, retrieve more results
-function showMoreVisible() {
-    let threshold, target = $("#showMoreResults");
-    if (!target.length) return;
-
-    threshold = $(window).scrollTop() + $(window).height() - target.height();
-    if (target.offset().top <= threshold) {
-        if (!target.data("visible")) {
-            target.data("visible", true);
-            Session.set("itemsLimit",
-                Session.get("itemsLimit") + ITEMS_INCREMENT);
-        }
-    } else {
-        if (target.data("visible")) {
-            target.data("visible", false);
-        }
-    }
+function showMoreVisible(){
+  let threshold, target = $(".showMoreResults");
+  if (!target.length) return;
+  threshold = $(window).scrollTop() + $(window).height() - target.height();
+  if (target.offset().top <= threshold) {
+    var becameVisible = jQuery.Event( "becameVisible" );
+    $( ".showMoreResults" ).trigger( becameVisible );
+  }
 }
 
 // run the above func every time the user scrolls
